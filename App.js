d1f55ced585e297c3aacaaf06dcd39b3cb442ecc
LipakshiBhatia <103330390+LipakshiBhatia@users.noreply.github.com>
2023-09-03 12:15:24 +0530

    App.js

--- App.js
+++ App.js
@@ -89,15 +89,3 @@ const App = () => {
 };
 
 export default App;
-
-//        _   _              _   _                   _
-//       | | | | ___ _   _  | |_| |__   ___ _ __ ___| |
-//       | |_| |/ _ \ | | | | __| '_ \ / _ \ '__/ _ \ |
-//       |  _  |  __/ |_| | | |_| | | |  __/ | |  __/_|
-//       |_| |_|\___|\__, |  \__|_| |_|\___|_|  \___(_)
-//                   |___/
-
-//       Lookin' at the source code huh?
-//       Be sure to drop me a line! I'd love to chat,
-//       whether it's about code, math,
-//       mythology, or anything in between.
